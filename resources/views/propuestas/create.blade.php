14fb1adcde7c87ad7e7ea4b384631cd3785367b0
Barbara Galindo Dorame <barbaragd@localhost.localdomain>
2019-05-29 16:17:29 -0700

    Agregar views o algo

--- resources/views/propuestas/create.blade.php
+++ resources/views/propuestas/create.blade.php
@@ -11,177 +11,244 @@
     <link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.min.css">
 
     <!-- Main css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/sass/style.css">
 </head>
 <body>
-  <main class="col-12">
-    <form method="post" action="{{ route('propuestas.store') }}">
-      <div class="form-row align-items-center">
-      <div class="form-group">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="nombre">Nombre completo</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-sm" name="nombre" placeholder="Fulanito de Tal">
+  <main class="container">
+    <h2>Crear propuesta de curso</h2>
+    <form method="post" action="{{ route('propuestas.store') }}" enctype="multipart/form-data">
+      @csrf
+      @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+      @endif
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="nombre">Nombre completo</label>
+              <input type="text" class="form-control" name="nombre" placeholder="Fulanito de Tal">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="correo">Correo electronico</label>
+            <input type="email" class="form-control" name="correo_instructor" placeholder="ejemplo@ejemplo.com">
+          </div>
         </div>
       </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="correo">Correo electronico</label>
-        <div class="col-sm-10">
-          <input type="email" class="form-control" name="correo" placeholder="ejemplo@ejemplo.com">
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="departamento">Departamento</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" name="departamento" placeholder="p.e. Ciencias naturales y exactas">
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="division">División</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" name="division" placeholder="p.e. Matemáticas">
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="tipo_curso">Tipo de curso</label>
-        <div class="col-sm-10">
-          <select class="form-control" name="tipo_curso">
-            <option>Obligatoria</option>
-            <option>Optativa</option>
-          </select>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="contenido">Contenido del curso</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="contenido" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="contenido_sintetico">Contenido sintetico del curso</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="contenido_sintetico" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="modulos">Modulos del curso</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="modulos" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="evaluacion">Metodo de evaluación</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="evaluacion" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="documentacion">Documentación</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="documentacion" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="financiamiento">Financiamiento</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="financiamiento" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="objetivo_general">Objetivo general</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="objetivo_general" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="objetivo_especifico">Objetivo específico</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="objetivo_especifico" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="utilidad">Utilidad del curso</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="utilidad" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="experiencia_intructor">Expeiencia del instructor</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="experiencia_intructor" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="curriculum_sintetico">Sintesis de curriculum</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="curriculum_sintetico" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="perfil_instructor">Perfil del instructor</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="perfil_instructor" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="operacion_curso">Operacion del curso</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="operacion_curso" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="unidad">Unidades</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="unidad" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="infraestructura">Infraestructura</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="infraestructura" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="requisitos_alumnos">Requisito para pasar el curso</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="requisitos_alumnos" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="exclusividad">Exclusividad</label>
-        <div class="col-sm-10">
+      <div class="row">
+        <div class="col-md-6">
+          <label class="col-form-label col-form-label-sm" for="exclusividad">Exclusividad</label>
           <select class="form-control" name="exclusividad">
             <option>Abierto</option>
             <option>Exclusivo</option>
           </select>
         </div>
-      </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="idioma">Idioma</label>
-        <div class="col-sm-10">
-          <select class="form-control" name="idioma">
-            <option>Español</option>
-            <option>Ingles</option>
-          </select>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-sm-2 col-form-label col-form-label-sm" for="idioma">Idioma</label>
+            <select class="form-control" name="idioma">
+              <option>Español</option>
+              <option>Ingles</option>
+            </select>
+          </div>
         </div>
       </div>
-      <div class="form-group col-md-6">
-        <label class="col-sm-2 col-form-label col-form-label-sm" for="requisitos_alumnos">Requisito para pasar el curso</label>
-        <div class="col-sm-10">
-        <textarea class="form-control" name="requisitos_alumnos" rows="3"></textarea>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="departamento">Departamento</label>
+            <input type="text" class="form-control" name="departamento" placeholder="p.e. Ciencias naturales y exactas">
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="division">División</label>
+            <input type="text" class="form-control" name="division" placeholder="p.e. Matemáticas">
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="tipo_curso">Tipo de curso</label>
+              <select class="form-control" name="tipo_curso">
+                <option>Obligatoria</option>
+                <option>Optativa</option>
+              </select>
+          </div>
         </div>
       </div>
-      <div class="col-auto">
-        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="contenido">Contenido del curso</label>
+            <textarea class="form-control" name="contenido" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="contenido_sintetico">Contenido sintetico del curso</label>
+            <textarea class="form-control" name="contenido_sintetico" rows="3"></textarea>
+          </div>
+        </div>
       </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="modulos">Modulos del curso</label>
+            <textarea class="form-control" name="modulos" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="evaluacion">Metodo de evaluación</label>
+            <textarea class="form-control" name="evaluacion" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="documentacion">Documentación</label>
+            <textarea class="form-control" name="documentacion" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="financiamiento">Financiamiento</label>
+            <textarea class="form-control" name="financiamiento" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="objetivo_general">Objetivo general</label>
+            <textarea class="form-control" name="objetivo_general" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="objetivo_especifico">Objetivo específico</label>
+            <textarea class="form-control" name="objetivo_especifico" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="utilidad">Utilidad del curso</label>
+            <textarea class="form-control" name="utilidad" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="experiencia_intructor">Expeiencia del instructor</label>
+            <textarea class="form-control" name="experiencia_instructor" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="perfil_instructor">Perfil del instructor</label>
+            <textarea class="form-control" name="perfil_instructor" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="curriculum_sintetico">Sintesis de curriculum</label>
+            <textarea class="form-control" name="curriculum_sintetico" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="operacion_curso">Operacion del curso</label>
+            <textarea class="form-control" name="operacion_curso" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="unidad">Unidades</label>
+            <textarea class="form-control" name="unidad" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="infraestructura">Infraestructura</label>
+            <textarea class="form-control" name="infraestructura" rows="3"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="requisitos_alumnos">Requisito para pasar el curso</label>
+            <textarea class="form-control" name="requisitos_alumnos" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="infraestructura">Cupo minimo</label>
+            <input type="number" class="form-control" name="cupo_minimo"></textarea>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="infraestructura">Cupo maximo</label>
+            <input type="number" class="form-control" name="cupo_maximo"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="infraestructura">Fecha inicio</label>
+            <input type="date" class="form-control" name="fecha_inicio"></textarea>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="infraestructura">Fecha fin</label>
+            <input type="date" class="form-control" name="fecha_final"></textarea>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label class="col-form-label col-form-label-sm" for="infraestructura">Duracion (horas)</label>
+            <input type="numeric" class="form-control" name="duracion_horas"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="form-group">
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" name="curriculum" id="curriculum">
+              <label class="custom-file-label" for="customFile">Choose file</label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-2">Submit</button>
+        </div>
       </div>
     </form>
   </main>
 
-    <!-- JS -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/jquery-ui/jquery-ui.min.js"></script>
-    <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
-    <script src="vendor/jquery-validation/dist/additional-methods.min.js"></script>
-    <script src="js/main.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
--- resources/views/propuestas/index.blade.php
+++ resources/views/propuestas/index.blade.php
@@ -11,14 +11,61 @@
     <link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.min.css">
 
     <!-- Main css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/sass/style.css">
 </head>
 <body>
-  HOLA MADAFAKA
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/jquery-ui/jquery-ui.min.js"></script>
-  <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
-  <script src="vendor/jquery-validation/dist/additional-methods.min.js"></script>
-  <script src="js/main.js"></script>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+      @if(session()->get('success'))
+        <div class="alert alert-success">
+          {{ session()->get('success') }}
+        </div><br />
+      @endif
+      </div>
+    </div>
+    <div class="row" style="margin-top:30px;">
+      <div class="col-12">
+        <a href="{{ route('propuestas.create') }}" class="btn btn-primary btn-lg" role="button" aria-pressed="true">Crear curso</a>
+
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-striped">
+          <thead>
+              <tr>
+                <td>ID</td>
+                <td>Nombre</td>
+                <td>Correo electronico</td>
+                <td colspan="2">Accion</td>
+              </tr>
+          </thead>
+          <tbody>
+              @foreach($propuestas as $propuesta)
+              <tr>
+                  <td>{{$propuesta->id}}</td>
+                  <td>{{$propuesta->nombre}}</td>
+                  <td>{{$Propuesta->correo_instructor}}</td>
+                  <td><a href="{{ route('propuestas.edit',$propuesta->id)}}" class="btn btn-primary">Edit</a></td>
+                  <td>
+                      <form action="{{ route('propuestas.destroy', $propuesta->id)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                      </form>
+                  </td>
+              </tr>
+              @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  <div>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
